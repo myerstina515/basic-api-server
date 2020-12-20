035952d2f8b188612d3044fa29d42ecc7fc1774e
TINA MYERS <myers.tina515@gmail.com>
2020-12-19 22:39:39 -0800

    working on tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -10,27 +10,45 @@ describe('web server', () => {
     expect(response.status).toBe(404);
   });
 
-  it('can add a record', async () => {
-    let mockRequest = 'purse';
-    const response = await mockRequest.post('/clothes');
-    // response.insert('purse');
-    expect(response).toBe('purse');
+  it('should respond with 404 on an invalid route', async () => {
+    const response = await mockRequest.get('/hello');
+    expect(response.status).toBe(404);
   });
 
   it('can get a list of records', async () => {
+    const response = await mockRequest.get('/clothes');
+    expect(response.status).toBe(200);
+    console.log(response.body);
+    expect(response.body).toEqual([]);
+  });
 
+  it('can create a record', async () => {
+    // let mockRequest = 'purse';
+    let record = {name: 'purse'};
+    const response = await mockRequest.post('/clothes').query(record);
+    // response.insert('purse');
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined;
   });
 
   it('can get a record', async () => {
-
+    const response = await mockRequest.get('/clothes').query(1);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([{'id': 1}]);
   });
 
   it('can update a record', async () => {
-
+    const object = { name: 'coat'};
+    const response = await mockRequest.put('/clothes/1').query(object);
+    expect(response.status).toBe(200);
+    // console.log(response.body);
+    expect(response.body).toBeDefined;
   });
 
   it('can delete a record', async () => {
-
+    const response = await mockRequest.delete('/clothes/1');
+    expect(response.body).toStrictEqual([]);
+    expect(response.status).toBe(200);
   });
 
 });
